9caefd2fc67e46a6f9a4b2084b0cb33de007e771
zcy_work <zuochengyao@megvii.com>
2018-06-28 18:16:12 +0800

    Site updated: 2018-06-28 18:16:12

---
+++ js/src/change-title.js
@@ -0,0 +1,4 @@
+var st, OriginTitile = document.title; 
+document.addEventListener("visibilitychange", function () { 
+    document.hidden ? (document.title = "Waiting for your return", clearTimeout(st)) : (document.title = "Hello world" + OriginTitile, st = setTimeout(function () { document.title = OriginTitile }, 4e3)); 
+})
